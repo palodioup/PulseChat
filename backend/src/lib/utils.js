4c0a2e61e4b31d4949aa98aab1323be42bc489b5
palodioup <dipoakanji57@gmail.com>
2026-04-26 19:53:22 +0100

    signup controller edit

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
+  res.cookie('jwt', token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true, // prevent XSS attacks 
+    sameSite: 'strict', // prevent CSRF attacks
+    secure: process.env.NODE_ENV === 'production' ? false : true // use secure cookies in production
+  });
+
+  return token;
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import { connectDB } from './lib/db.js';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
@@ -9,6 +10,8 @@ const __dirname = path.resolve();
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
@@ -24,4 +27,5 @@ if (process.env.NODE_ENV === 'production') {
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 });
