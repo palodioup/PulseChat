0e611a254f7ec8daeacc525aca9ffae1e6d72161
palodioup <dipoakanji57@gmail.com>
2026-04-29 17:27:11 +0100

    message routes added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,8 +1,15 @@
-import express from 'express';
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js"
+import { getAllContacts, getMessagesByUserId , getChatPartners, sendMessage} from "../controllers/message.controller.js";
+
 const router = express.Router();
+router.use(protectRoute)
 
-router.get('/send', (req, res) => {
-    res.send('Send message');
-});
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners)
+router.get(":/id", getMessagesByUserId)
+router.post("/send/:id", sendMessage)
 
-export default router
+
+export default router;
